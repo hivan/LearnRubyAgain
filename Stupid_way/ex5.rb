3d92f669fa4f8fbcfa99fee09078cd22a13929a6
Hivan Du <doo@hivan.me>
2015-01-21 17:53:56 +0800

    update to ex15.rb

--- Stupid_way/ex5.rb
+++ Stupid_way/ex5.rb
@@ -10,7 +10,7 @@ puts "Let's talk about %s." % my_name
 puts "He's %d inches tall." % my_height
 puts "He's %d pounds heavy." % my_weight
 puts "Actually that's not too heavy."
-puts "He's got %s eyes and %s hair." %[my_eyes, my_hair]
+puts "He's got %s eyes and %s hair." % [my_eyes, my_hair]
 puts "His teeth are usually %s depending on the coffee." % my_teeth
 
 # this line is tricky, try to get it exactly right
